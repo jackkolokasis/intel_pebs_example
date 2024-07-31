f42b1911d2affa4012ce6d910059395e38695af3
Jack Kolokasis <kolokasis@ics.forth.gr>
2024-07-31 18:11:32 +0300

    [fix] Fix itentation

--- src/count_stores.c
+++ src/count_stores.c
@@ -8,49 +8,49 @@
 #include <stdint.h>
 
 static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid, int cpu, int group_fd, unsigned long flags) {
-    return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
+  return syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
 }
 
 void store_memory(int *array, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        array[i] = i;
-    }
+  for (size_t i = 0; i < size; i++) {
+    array[i] = i;
+  }
 }
 
 int main() {
-    struct perf_event_attr pe;
-    long long count;
-    int fd;
+  struct perf_event_attr pe;
+  long long count;
+  int fd;
 
-    memset(&pe, 0, sizeof(struct perf_event_attr));
-    pe.type = PERF_TYPE_RAW;
-    pe.size = sizeof(struct perf_event_attr);
-    pe.config = 0x82d0; // Event code for MEM_UOPS_RETIRED.ALL_STORES
-    pe.disabled = 1;
-    pe.exclude_kernel = 1;
-    pe.exclude_hv = 1;
+  memset(&pe, 0, sizeof(struct perf_event_attr));
+  pe.type = PERF_TYPE_RAW;
+  pe.size = sizeof(struct perf_event_attr);
+  pe.config = 0x82d0; // Event code for MEM_UOPS_RETIRED.ALL_STORES
+  pe.disabled = 1;
+  pe.exclude_kernel = 1;
+  pe.exclude_hv = 1;
 
-    fd = perf_event_open(&pe, 0, -1, -1, 0);
-    if (fd == -1) {
-        perror("perf_event_open");
-        exit(EXIT_FAILURE);
-    }
+  fd = perf_event_open(&pe, 0, -1, -1, 0);
+  if (fd == -1) {
+    perror("perf_event_open");
+    exit(EXIT_FAILURE);
+  }
 
-    size_t size = 1000000;
-    int *array = malloc(size * sizeof(int));
+  size_t size = 1000000;
+  int *array = malloc(size * sizeof(int));
 
-    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+  ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+  ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
 
-    store_memory(array, size);
+  store_memory(array, size);
 
-    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
-    read(fd, &count, sizeof(long long));
+  ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+  read(fd, &count, sizeof(long long));
 
-    printf("Number of store operations: %lld\n", count);
+  printf("Number of store operations: %lld\n", count);
 
-    close(fd);
-    free(array);
+  close(fd);
+  free(array);
 
-    return 0;
+  return 0;
 }
